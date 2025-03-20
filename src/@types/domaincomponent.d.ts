5917f6ffa9a580f8ee18a46495edbdf49834cf07
Wei Jun Leong <ambroseleong04@gmail.com>
2025-03-20 08:15:11 +0800

    update

---
+++ src/@types/domaincomponent.d.ts
@@ -0,0 +1,17 @@
+declare module "domaincomponent" {
+    export function myFunction(): string;
+    export const DomainTextInput: any;
+    export const DomainLoading: any;
+    export const DomainSwitchButton: any;
+    export const DomainSelect: any;
+    export const DomainMainContainer: any;
+    export const DomainModal: any;
+    export const DomainCheckbox: any;
+    export const DomainRadioGroup: any;
+    export const DomainSlider: any;
+    export const DomainHelperText: any;
+    export const DomainDropdownMenu: any;
+    export const DomainHeader: any;
+    export const DomainAlert: any;
+    export function showAlert(): void;
+}
